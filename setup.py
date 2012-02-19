47b1947b7755a06875e9ec0ac01db3c3f26a8e50
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-02-19 15:20:27 +0100

    Explicitely set package name and location, add data files

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 version = '0.1.0'
 name = 'websockify'
@@ -18,13 +18,15 @@ setup(name=name,
       author="Joel Martin",
       author_email="github@martintribe.org",
 
-      packages=find_packages(),
+      packages=['websockify'],
+      package_dir={'websockify': '.'},
+      package_data={'websockify': ['include/*.js', 'include/web-socket-js/*']},
       include_package_data=True,
       install_requires=['numpy'],
       zip_safe=False,
       entry_points={
         'console_scripts': [
-          'websockify = websockify:websockify_init',
+          'websockify = websockify.websockify:websockify_init',
         ]
       },
     )
